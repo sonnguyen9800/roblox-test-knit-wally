81474ba3c6dd45c6e66b2622c89f4bae80dd4815
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2022-07-31 12:29:06 +0700

    Update

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,1 +1,11 @@
-print("Hello world, from client!")
+local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
+
+Knit.Start():catch(warn)
+
+for _, module in pairs(script:GetChildren()) do
+    local loadMod = coroutine.create(function()
+        require(module);
+    end)
+
+    coroutine.resume(loadMod);
+end
--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,11 @@
-print("Hello world, from server!")
+local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
+
+Knit.Start():catch(warn)
+
+for _, module in pairs(script:GetChildren()) do
+    local loadMod = coroutine.create(function()
+        require(module);
+    end)
+
+    coroutine.resume(loadMod);
+end
